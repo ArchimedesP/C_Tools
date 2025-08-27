61335b706cd1fa0a84b6927e0d8bfad8c2e39b95
ArchimedesP <andrzejprzedworski1@gmail.com>
2025-08-27 15:31:55 -0400

    Update Drawing.h

--- Drawing.h
+++ Drawing.h
@@ -2,8 +2,20 @@
 
 #pragma once
 
-// contains my custom drawing commands for winapi graphics
+#define string_maxLength 128
 
+#define RGB(r, g, b)	(r | (g << 8) | (b << 16))
+#define GetRValue(rgb)	(rgb & 0xff)
+#define GetGValue(rgb)	( (((rgb)) >> 8) & 0xff )
+#define GetBValue(rgb)	( ((rgb) >> 16) & 0xff )
+
+#define RGB_BLEND(color1, color2, amount)	RGB(\
+(int)(GetRValue(color1) * (1 - amount / 100.0) + GetRValue(color2) * (amount / 100.0)),\
+(int)(GetGValue(color1) * (1 - amount / 100.0) + GetGValue(color2) * (amount / 100.0)),\
+(int)(GetBValue(color1) * (1 - amount / 100.0) + GetBValue(color2) * (amount / 100.0))\
+)
+
+// contains drawing commands for winapi graphics
 
 struct frameStruct {
 	int width;
@@ -12,7 +24,7 @@ struct frameStruct {
 };
 
 
-void frame_DrawText(struct frameStruct* frame, int x1, int y1, char text[], int text_xScale, int text_yScale, int color);
+void frame_DrawText(struct frameStruct* frame, int x1, int y1, char text[], int text_xScale, int text_yScale, int color, float opacity);
 
 void frame_DrawLine(struct frameStruct* frame, int x1, int y1, int x2, int y2, int color);
 
@@ -22,12 +34,14 @@ void frame_DrawLineA(struct frameStruct* frame, int x1, int y1, int dist, double
 
 void frame_DrawRect(struct frameStruct* frame, int x1, int y1, int x2, int y2, int color);
 
-void frame_DrawRectFilled(struct frameStruct* frame, int x1, int y1, int x2, int y2, int color);
+void frame_DrawRectFilled(struct frameStruct* frame, int x1, int y1, int x2, int y2, int color, float opacity);
 
 void frame_DrawRectA(struct frameStruct* frame, int xAnchor, int yAnchor, int horizontalShift, int verticalShift, double angle, int width, int height, int color);
 
 void frame_DrawRectAFilled(struct frameStruct* frame, int xAnchor, int yAnchor, int horizontalShift, int verticalShift, int angle, int width, int height, int color);
 
-void frame_DrawCircleFilled(struct frameStruct* frame, int x1, int y1, int radius, int color);
+void frame_DrawCircleFilled(struct frameStruct* frame, int x1, int y1, int radius, int color, float opacity);
+
+void frame_DrawSemiCircleFilled(struct frameStruct* frame, int x1, int y1, int radius, int sliceAngle, int angle, int color, float opacity);
 
 void frame_DrawTriangleFilled(struct frameStruct* frame, int x1, int y1, int x2, int y2, int x3, int y3, int color);
